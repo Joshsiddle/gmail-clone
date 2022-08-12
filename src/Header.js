0fa1fa6a2d59b3fdaf4b1bf11e3bb03d3badad49
Joshua Siddle <106534417+Joshsiddle@users.noreply.github.com>
2022-08-12 14:11:56 +0100

    sidebar

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./Header.css";
 import MenuIcon from "@mui/icons-material/Menu";
-import { IconButton } from "@mui/material";
+import { Avatar, IconButton } from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search'
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
+import NotificationsIcon from '@mui/icons-material/Notifications'
+import AppsIcon from '@mui/icons-material/Apps'
 
 function Header() {
   return (
@@ -12,8 +16,22 @@ function Header() {
         </IconButton>
         <img src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png" />
       </div>
-      <div className="header__middle"></div>
-      <div className="header__right"></div>
+      
+      <div className="header__middle">
+        <SearchIcon />
+        <input placeholder="Search mail" type='text' />
+        <ArrowDropDownIcon className='header__inputCaret' />
+      </div>
+      
+      <div className="header__right">
+        <IconButton>
+            <AppsIcon />
+        </IconButton>
+        <IconButton>
+            <NotificationsIcon />
+        </IconButton>
+        <Avatar />
+      </div>
     </div>
   );
 }
